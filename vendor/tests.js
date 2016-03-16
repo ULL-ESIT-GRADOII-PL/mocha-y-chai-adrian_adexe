10576b43a2b98e52061a8103fdd2f15a5200ffb8
Adexe Sabina <alu0100769609@ull.edu.es>
2016-03-16 19:19:36 +0000

    Arreglando fallos ocasionados por el merge

--- vendor/tests.js
+++ vendor/tests.js
@@ -10,46 +10,47 @@ describe("Creación de un objeto Medida", function(){
       expect(medida.tipo).to.equal('F');
    });
 });
+
 describe("Creación de un objeto Temperatura", function(){
+   var temperatura = new Temperatura(23,'F');
    it("Debe tener un número", function(){
-      var temperatura = new Temperatura(23,'F');
       expect(temperatura.valor).to.equal(23);
    });
    it("Debe tener una unidad de medida", function(){
-      var temperatura = new Temperatura(55,'K');
-      expect(temperatura.tipo).to.equal('K');
-   });
-
-   describe("Creación de un objeto Kelvin", function(){
-      var kelvin = new Kelvin(2);
-      it("Debe tener un número", function(){
-         expect(kelvin.valor).to.equal(2);
-      });
-      it("Debe tener una unidad de medida", function(){
-         expect(kelvin.tipo).to.equal('k');
-      });
-   });
-
-   describe("Creación de un objeto Celsius", function(){
-      var celsius = new Celsius(3);
-      it("Debe tener un número", function(){
-         expect(celsius.valor).to.equal(3);
-      });
-      it("Debe tener una unidad de medida", function(){
-         expect(celsius.tipo).to.equal('c');
-      });
-   });
-
-   describe("Creación de un objeto Farenheit", function(){
-      var farenheit = new Farenheit(10);
-      it("Debe tener un número", function(){
-         expect(farenheit.valor).to.equal(10);
-      });
-      it("Debe tener una unidad de medida", function(){
-         expect(farenheit.tipo).to.equal('f');
-      });
+      expect(temperatura.tipo).to.equal('F');
    });
 });
+
+describe("Creación de un objeto Kelvin", function(){
+   var kelvin = new Kelvin(2);
+   it("Debe tener un número", function(){
+      expect(kelvin.valor).to.equal(2);
+   });
+   it("Debe tener una unidad de medida", function(){
+      expect(kelvin.tipo).to.equal('k');
+   });
+});
+
+describe("Creación de un objeto Celsius", function(){
+   var celsius = new Celsius(3);
+   it("Debe tener un número", function(){
+      expect(celsius.valor).to.equal(3);
+   });
+   it("Debe tener una unidad de medida", function(){
+      expect(celsius.tipo).to.equal('c');
+   });
+});
+
+describe("Creación de un objeto Farenheit", function(){
+   var farenheit = new Farenheit(10);
+   it("Debe tener un número", function(){
+      expect(farenheit.valor).to.equal(10);
+   });
+   it("Debe tener una unidad de medida", function(){
+      expect(farenheit.tipo).to.equal('f');
+   });
+});
+
 describe("Conversión de temperaturas", function(){
    describe("De celsius a kelvin", function(){
       var celsius = new Celsius(23);
