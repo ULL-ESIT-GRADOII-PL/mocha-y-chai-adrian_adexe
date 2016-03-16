97cd21187a9fba969d8b76135d2fc068aed71f47
Adrian <alu0100288216@ull.edu.es>
2016-03-16 19:12:24 +0000

    Tests para la creación de objetos de las clases Kelvin, Celsius y Farenheit.

--- vendor/tests.js
+++ vendor/tests.js
@@ -11,4 +11,34 @@ describe("Temperatura",function() {
          expect(medida.tipo).to.equal('F');
       });
    });
+
+   describe("Creación de un objeto Kelvin", function(){
+      var kelvin = new Kelvin(2);
+      it("Debe tener un número", function(){
+         expect(kelvin.valor).to.equal(2);
+      });
+      it("Debe tener una unidad de medida", function(){
+         expect(kelvin.tipo).to.equal('k');
+      });
+   });
+
+   describe("Creación de un objeto Celsius", function(){
+      var celsius = new Celsius(3);
+      it("Debe tener un número", function(){
+         expect(celsius.valor).to.equal(3);
+      });
+      it("Debe tener una unidad de medida", function(){
+         expect(celsius.tipo).to.equal('c');
+      });
+   });
+
+   describe("Creación de un objeto Farenheit", function(){
+      var farenheit = new Farenheit(10);
+      it("Debe tener un número", function(){
+         expect(farenheit.valor).to.equal(10);
+      });
+      it("Debe tener una unidad de medida", function(){
+         expect(farenheit.tipo).to.equal('f');
+      });
+   });
 });
