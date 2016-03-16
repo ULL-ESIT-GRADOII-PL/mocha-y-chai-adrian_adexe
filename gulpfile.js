c8cd810438c02fe8acbc0ff365e26b67f8b2e129
Adexe Sabina <alu0100769609@ull.edu.es>
2016-03-16 20:41:01 +0000

    Arreglando gulpfile.js y borrando fichero perdido

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,15 @@ var minifyCSS  = require('gulp-minify-css');
 gulp.task('minify', function () {
   gulp.src('assets/js/*.js')
   .pipe(uglify())
-  .pipe(gulp.dest('minified'));
+  .pipe(gulp.dest('minified/javascript/'));
 
   gulp.src('./index.html')
     .pipe(minifyHTML())
     .pipe(gulp.dest('./minified/'))
 
-  gulp.src('./css/*.css')
+  gulp.src('assets/css/*.css')
    .pipe(minifyCSS({keepBreaks:true}))
-   .pipe(gulp.dest('./minified/'))
+   .pipe(gulp.dest('./minified/stylesheet/'))
 });
 
 gulp.task('clean', function(cb) {
